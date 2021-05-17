4346a84313808dacc45bba2bd8b38f38b91bb580
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2021-05-17 13:07:29 -0500

    update

--- src/App.js
+++ src/App.js
@@ -5,38 +5,96 @@ import Display from "./Display";
 import Form from "./Form";
 
 function App() {
-  const [dog,setDog] = useState([])
-  
-  const url = 'http://localhost:4500'
-  useEffect(()=>{
-    (async ()=>{
-      const response = await fetch(url + '/dog/')
-      const data = await response.json()
-       setDog(data)
-       console.log(data)
+  const [dogs,setDogs] = useState([])
+  const emptyDog = {
+    name : '',
+    age :0,
+    img: ""
 
-    })()
+  }
+  const [selectedDog, setSelectedDog] = useState(emptyDog)
+  const url = 'http://localhost:4500'
+
+    // Function to get list of Dogs
+    const getDogs = () => {
+      // make a get a request to this url
+      fetch(url + "/dog/")
+      // use .then to take action when the response comes in
+      // convert data into js object
+      .then((response) => response.json())
+      // use the data from the response
+      .then((data) => {
+        setDogs(data)
+      })
+      }
+  useEffect(()=>{
+   getDogs()
   },[])
+  const handleCreate = (newDog)=> {
+  fetch(url + '/dog/',{
+    method : "POST",
+    headers: {
+      "Content-Type": "application/json",
+   },
+    body: JSON.stringify(newDog)
+  })
+  .then(()=> {})
+  }
+
+  const handleUpdate = (dog) => {
+    fetch(url + "/dog/" + dog._id, {
+      method: "PUT",
+      headers: {
+        "Content-Type":"application/json"
+      },
+      body: JSON.stringify(dog)
+    })
+    .then(() => getDogs())
+  }
+
+  //function to specifi a dog
+  // function to specify which dog we are updated
+const selectDog = (dog) => {
+  setSelectedDog(dog)
+}
 
   return (
     <div className="App">
       <h1>DOG LISTING SITE</h1>
       <hr />
+      <Link to='/create'>
+        <button>Add Dog</button>
+      </Link>
       <main>
-        <Switch>
-          <Route exact path="/" render={(rp) => <Display {...rp} />} />
+      <Switch>
+          <Route
+            exact
+            path="/"
+            render={(rp) => (
+              <Display {...rp} dogs={dogs} selectDog={selectDog} />
+            )}
+          />
           <Route
             exact
             path="/create"
             render={(rp) => (
-              <Form {...rp} label="create" dog={{}} handleSubmit={() => {}} />
+              <Form
+                {...rp}
+                label="create"
+                dog={emptyDog}
+                handleSubmit={handleCreate}
+              />
             )}
           />
           <Route
             exact
             path="/edit"
             render={(rp) => (
-              <Form {...rp} label="update" dog={{}} handleSubmit={() => {}} />
+              <Form 
+              {...rp} 
+              label="update" 
+              dog={selectedDog} 
+              handleSubmit={handleUpdate} />
             )}
           />
         </Switch>
--- src/Display.js
+++ src/Display.js
@@ -1,8 +1,25 @@
 import React from "react";
 
-const Display = (props) => {
-  console.log(props)
-  return <h1>Display</h1>;
+const Display = ({dogs,selectDog,history}) => {
+  const loaded =() => {
+    return <div className="dogs">
+  
+    {dogs.map((item,i)=>{
+     return(<div key={i} className='dog'>
+             <h2>Name : {item.name}</h2>
+              <img src={item.img} alt={item.name}/>
+              <h3>Age : {item.age}</h3>
+              <button onClick={() => {
+            selectDog(item)
+            history.push("/edit")
+          }}>
+            edit
+          </button>
+            </div>)})}
+          </div>
+  }
+  const loading =() => <h3>Loading...</h3>
+  return dogs ? loaded() :loading();
 };
 
 export default Display;
